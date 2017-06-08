6737f1fc0e12925ceb5cd18fe2d1d29e2ccce014
Julien Perret <julienperret01@gmail.com>
2017-06-08 10:23:22 +0200

    Revert "fail"
    This reverts commit 805484cd06d2adf3531e5543ef9ac30bf8efa06d.

--- src/bbman/main.java
+++ src/bbman/main.java
@@ -7,201 +7,126 @@ import java.awt.Color;
 public class main 
 {	
 	public static void main(String[] args) 
-	{	int nb_col=21;
-		int nb_line=17;
-		int size_x=25;
-		int size_y=25;
-		int nb_joueur=2;
-		
-		int i=0;
-		int j=0;
-		
-		int rec=0;
-		
+	{	
+		int numberOfRow=21;
+		int numberOfLine=17;
+		int halfWidthOfRow=25;
+		int halfHeigthOfLine=25;
+		int numberOfPlayers=2;
 
-		StdDraw.setCanvasSize (size_x*nb_col*2,size_y*nb_line*2);
+		StdDraw.setCanvasSize(halfWidthOfRow*numberOfRow*2,halfHeigthOfLine*numberOfLine*2);
 	
-		StdDraw.setXscale(0, size_x*nb_col*2);
-		StdDraw.setYscale(0, size_y*nb_line*2);
+		StdDraw.setXscale(0, halfWidthOfRow*numberOfRow*2);
+		StdDraw.setYscale(0, halfHeigthOfLine*numberOfLine*2);
 		
 		StdDraw.show(0);
 
-		Terrain terrain=new Terrain(nb_col,nb_line,size_x,size_y);
-		Joueur [] joueur=new Joueur [nb_joueur] ;
-	
-		for (i=0;i<nb_joueur;i++)
-		{	Color color =new Color (255,50,50+i);
-			joueur [i]=new Joueur(terrain,color);
-			joueur [i].init(i+1,nb_col,nb_line,size_x,size_y);
+		Ground ground=new Ground(numberOfRow,numberOfLine,halfWidthOfRow,halfHeigthOfLine); // instanciation du ground
+		Player [] player=new Player [numberOfPlayers] ;
+		
+		int idplayer;
+		int positionX;
+		int positionY;
+		
+		for (int i=0;i<numberOfPlayers;i++)
+		{	
+			idplayer = i;
+			positionX=0;
+			positionY=0;
+			if(idplayer==0)
+			{
+				positionX=3*halfWidthOfRow;
+				positionY=3*halfHeigthOfLine;
+			}
+			else if(idplayer==1)
+			{
+				positionX=(numberOfRow*(halfWidthOfRow*2))-(3*halfWidthOfRow);
+				positionY=(numberOfLine*(halfHeigthOfLine*2))-(3*halfHeigthOfLine);
+			}
+			player[i]=new Player(ground,idplayer, positionX, positionY);	// création des players
 		}
 		
-		terrain.draw_all (joueur, nb_joueur);
+		ground.draw(player); // on dessine le début de partie
 
-		while (noPlayerIsDead(joueur))
-		{	rec=0;
-			i=0;
-			j=0;
-		
-			rec=move2 (joueur, nb_joueur,terrain);
+		while (noPlayerIsDead(player)) // si aucun player n'est mort
+		{	
+			listenToPlayersAction(player, ground); // on écoute les saisis des deux players
 			
-			
-			for (i=0; i<nb_joueur; i++)
-			{	terrain=joueur[i].bon_deg (terrain);
-				for (j=0; j<joueur[i].getnbbombe();j++)
-					terrain=joueur[i].bombe[j].gestion(terrain);
+			for (int i=0; i<numberOfPlayers; i++)
+			{	
+				ground=player[i].getBonus(ground); // on regarde si le player est sur une case avec un bonus
+				for (int j=0; j<player[i].getNumberOfBomb();j++)
+				{
+					ground=player[i].bombe[j].manage(ground, player); // on gère les bombes
+				}
 			}
 		
-			terrain.draw_all (joueur, nb_joueur);
-			
-			clear();
-			debug(joueur, nb_joueur);
-			
+			ground.draw(player); // on dessine le tout
 			StdDraw.show();
-			sleep (5);
+			pause(5);
 		}
-		
-		displayGameOver(joueur, terrain);
-	}
-	public static void clear ()
-	{	
-		System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	}
-	
-	public static void sleep (int mili)
-	{	long time=java.lang.System.currentTimeMillis() ;
-	
-		while (java.lang.System.currentTimeMillis()-time<mili);
-	}
-	
-	public static void debug (Joueur [] joueur, int nb)
-	{
-		int i;
-		int j;
-		int nbj;
-		
-		int compte;
-		
-		for (i=0;i<nb;i++)
-		{	compte=0;
-			for (j=0;j<joueur[i].getnbbomb();j++)
-			{	if (joueur[i].getbomb(j).getactivate()==0)
-					compte=compte+1;
-			}
-			nbj=i+1;
-			System.out.print("	JOUEUR "+nbj+":\nLIFE :"+joueur[i].getlife()+"\nBOMB: "+compte+"\n\n\n");
-		}
+		ground.displayGameOver(player, ground); // si un player est mort on affiche l'écran de fin
 	}
 
-	public static int move2 (Joueur [] joueur, int nb_joueur, Terrain terrain)
-	{	int ret=0;
-	
+	public static void listenToPlayersAction(Player [] player, Ground ground)
+	{	
 		if (StdDraw.isKeyPressed(KeyEvent.VK_Z))
-		{	joueur[0].move(1,terrain);
-			ret=1;
+		{	
+			player[0].moveTo("up",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_Q))
-		{	joueur[0].move(2,terrain);
-			ret=1;
+		{	
+			player[0].moveTo("left",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_S))
-		{	joueur[0].move(3,terrain);
-			ret=1;
+		{	
+			player[0].moveTo("down",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_D))
-		{	joueur[0].move(4,terrain);
-			ret=1;
+		{	
+			player[0].moveTo("right",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_A))
-		{	terrain=joueur[0].put_bombe(terrain);
-			ret=1;
-			
+		{	
+			ground=player[0].dropBomb(ground);	
 		}
-		
 		if (StdDraw.isKeyPressed(KeyEvent.VK_UP))
-		{	joueur[1].move(1,terrain);
-			ret=1;
+		{	
+			player[1].moveTo("up",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT))
-		{	joueur[1].move(2,terrain);
-			ret=1;
+		{	
+			player[1].moveTo("left",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN))
-		{	joueur[1].move(3,terrain);
-			ret=1;
+		{	
+			player[1].moveTo("down",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT))
-		{	joueur[1].move(4,terrain);
-			ret=1;
+		{	
+			player[1].moveTo("right",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_ENTER))
 		{	
-			terrain=joueur[1].put_bombe(terrain);
-			ret=1;
+			ground=player[1].dropBomb(ground);
 		}
-		
-		return ret;
+	}
+	
+	public static void pause(int mili)
+	{	
+		long time=java.lang.System.currentTimeMillis();
+		while (java.lang.System.currentTimeMillis()-time<mili);
 	}
 
-	
-	public static boolean noPlayerIsDead(Joueur[] joueur)
+	public static boolean noPlayerIsDead(Player[] player)
 	{
-		for (int i = 0 ; i < joueur.length ; i++)
+		for (int i = 0 ; i < player.length ; i++)
 		{
-			if(joueur[i].getlife()<=0)
+			if(player[i].getNumberOfLife()<=0)
 			{
 				return false;
 			}
 		}
 		return true;
-	}
-	
-	public static void displayGameOver(Joueur[] joueur, Terrain terrain)
-	{
-		String joueurGagnant;
-		if(joueur[0].getlife()<=0)
-		{
-			joueurGagnant = "Joueur2" ;
-		}
-		else
-		{
-			joueurGagnant="Joueur1";
-
-		}
-		
-		if (joueurGagnant=="Joueur1")
-		{
-			StdDraw.picture(terrain.getwidth()*2*11, terrain.getheigth()*2*8, "FinJ1.png", 500, 300);
-			StdDraw.picture(terrain.getwidth()*2*10, terrain.getheigth()*2*5.8, "Rejouer.png", 100, 50);
-			Audio sound = new Audio("Violin");
-		}
-		else if (joueurGagnant=="Joueur2")
-		{
-			
-			StdDraw.picture(terrain.getwidth()*2*11, terrain.getheigth()*2*8, "FinJ2.png", 500, 300);
-			StdDraw.picture(terrain.getwidth()*2*10, terrain.getheigth()*2*5.8, "Rejouer.png", 100, 50);
-			Audio sound = new Audio("Hello");
-			
-		}
-		
-		
-		StdDraw.show();
-		
-		while(true)
-		{
-			if(StdDraw.mousePressed())
-			{
-				if(StdDraw.mouseX()>terrain.getwidth()*2*10-100
-				&& StdDraw.mouseX()<terrain.getwidth()*2*10+100
-				&& StdDraw.mouseY()>terrain.getheigth()*2*5.8-50
-				&& StdDraw.mouseY()<terrain.getheigth()*2*5.8+50)
-					
-				{
-					System.out.println("Clicked");
-					main(null);
-				}
-			}
-		}
-		
-
 	}
 }
