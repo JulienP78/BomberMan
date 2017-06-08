77768d79e9f37a52e39a040a58e5684750e96d79
Julien Perret <julienperret01@gmail.com>
2017-06-08 10:42:05 +0200

    renomme

--- src/bbman/main.java
+++ src/bbman/main.java
@@ -22,93 +22,93 @@ public class main
 		StdDraw.show(0);
 
 		Ground ground=new Ground(numberOfRow,numberOfLine,halfWidthOfRow,halfHeigthOfLine); // instanciation du ground
-		Player [] joueur=new Player [numberOfPlayers] ;
+		Player [] players=new Player [numberOfPlayers] ;
 		
-		int idJoueur;
+		int idPlayer;
 		int positionX;
 		int positionY;
 		
 		for (int i=0;i<numberOfPlayers;i++)
 		{	
-			idJoueur = i;
+			idPlayer = i;
 			positionX=0;
 			positionY=0;
-			if(idJoueur==0)
+			if(idPlayer==0)
 			{
 				positionX=3*halfWidthOfRow;
 				positionY=3*halfHeigthOfLine;
 			}
-			else if(idJoueur==1)
+			else if(idPlayer==1)
 			{
 				positionX=(numberOfRow*(halfWidthOfRow*2))-(3*halfWidthOfRow);
 				positionY=(numberOfLine*(halfHeigthOfLine*2))-(3*halfHeigthOfLine);
 			}
-			joueur[i]=new Player(ground,idJoueur, positionX, positionY);	// création des joueurs
+			players[i]=new Player(ground,idPlayer, positionX, positionY);	// création des joueurs
 		}
 		
-		ground.draw(joueur); // on dessine le début de partie
+		ground.draw(players); // on dessine le début de partie
 
-		while (noPlayerIsDead(joueur)) // si aucun joueur n'est mort
+		while (noPlayerIsDead(players)) // si aucun joueur n'est mort
 		{	
-			listenToPlayersAction(joueur, ground); // on écoute les saisis des deux joueurs
+			listenToPlayersAction(players, ground); // on écoute les saisis des deux joueurs
 			
 			for (int i=0; i<numberOfPlayers; i++)
 			{	
-				ground=joueur[i].getBonus(ground); // on regarde si le joueur est sur une case avec un bonus
-				for (int j=0; j<joueur[i].getNumberOfBomb();j++)
+				ground=players[i].getBonus(ground); // on regarde si le joueur est sur une case avec un bonus
+				for (int j=0; j<players[i].getNumberOfBomb();j++)
 				{
-					ground=joueur[i].bombe[j].manage(ground, joueur); // on gère les bombes
+					ground=players[i].bombe[j].manage(ground, players); // on gère les bombes
 				}
 			}
 		
-			ground.draw(joueur); // on dessine le tout
+			ground.draw(players); // on dessine le tout
 			StdDraw.show();
 			pause(5);
 		}
-		ground.displayGameOver(joueur, ground); // si un joueur est mort on affiche l'écran de fin
+		ground.displayGameOver(players, ground); // si un joueur est mort on affiche l'écran de fin
 	}
 
-	public static void listenToPlayersAction(Player [] joueur, Ground ground)
+	public static void listenToPlayersAction(Player [] players, Ground ground)
 	{	
 		if (StdDraw.isKeyPressed(KeyEvent.VK_Z))
 		{	
-			joueur[0].moveTo("up",ground);
+			players[0].moveTo("up",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_Q))
 		{	
-			joueur[0].moveTo("left",ground);
+			players[0].moveTo("left",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_S))
 		{	
-			joueur[0].moveTo("down",ground);
+			players[0].moveTo("down",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_D))
 		{	
-			joueur[0].moveTo("right",ground);
+			players[0].moveTo("right",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_A))
 		{	
-			ground=joueur[0].dropBomb(ground);	
+			ground=players[0].dropBomb(ground);	
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_UP))
 		{	
-			joueur[1].moveTo("up",ground);
+			players[1].moveTo("up",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT))
 		{	
-			joueur[1].moveTo("left",ground);
+			players[1].moveTo("left",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN))
 		{	
-			joueur[1].moveTo("down",ground);
+			players[1].moveTo("down",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT))
 		{	
-			joueur[1].moveTo("right",ground);
+			players[1].moveTo("right",ground);
 		}
 		if (StdDraw.isKeyPressed(KeyEvent.VK_ENTER))
 		{	
-			ground=joueur[1].dropBomb(ground);
+			ground=players[1].dropBomb(ground);
 		}
 	}
 	
@@ -118,11 +118,11 @@ public class main
 		while (java.lang.System.currentTimeMillis()-time<mili);
 	}
 
-	public static boolean noPlayerIsDead(Player[] joueur)
+	public static boolean noPlayerIsDead(Player[] players)
 	{
-		for (int i = 0 ; i < joueur.length ; i++)
+		for (int i = 0 ; i < players.length ; i++)
 		{
-			if(joueur[i].getNumberOfLife()<=0)
+			if(players[i].getNumberOfLife()<=0)
 			{
 				return false;
 			}
